df8fa5e19021363123c57b2447e324e04e3670c0
match7257 <moonlight837462@andline-05.net>
2026-08-10 17:52:26 +0900

    画像の表示

--- js/main.js
+++ js/main.js
@@ -26,11 +26,11 @@
   new Tabs('.tab-btn', '.tab-content');
 
 
-  // document.querySelectorAll('.work-toggle').forEach(btn => {
-  //   btn.addEventListener('click', () => {
-  //     btn.classList.toggle('active');
-  //   });
-  // });
+  document.querySelectorAll('.work-toggle').forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('active');
+    });
+  });
 
 
 }
