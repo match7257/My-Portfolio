f3eb1720d441641e49dbd065d2be060e0c59b3ff
match7257 <moonlight837462@andline-05.net>
2026-08-10 17:16:53 +0900

    ボタンの削除

--- js/main.js
+++ js/main.js
@@ -26,11 +26,12 @@
   new Tabs('.tab-btn', '.tab-content');
 
 
-  document.querySelectorAll('.work-toggle').forEach(btn => {
-    btn.addEventListener('click', () => {
-      btn.classList.toggle('active');
-    });
-  });
+  // document.querySelectorAll('.work-toggle').forEach(btn => {
+  //   btn.addEventListener('click', () => {
+  //     btn.classList.toggle('active');
+  //   });
+  // });
 
 
 }
+
